b6902cdcadcddc79ac225f35945c0417c9c49671
Juan Ignacio Rago <juanrago2015@gmail.com>
2021-11-29 12:11:38 -0300

    Se agregó en la clase Producto comentarios y arreglos en los setters

--- codigo/TP3-ej6/Producto.py
+++ codigo/TP3-ej6/Producto.py
@@ -1,42 +1,56 @@
 from datetime import date
 
+""" Clase Producto en Python"""
+
 class Producto():
-    __nombre: str
+
+    __nombre: str                             # Atributos privados 
     __fechaVencimiento: date
     __nroLote: int
     __fechaEnvasado: date
     __granjaOrigen: str
 
-    def __init__(self, nombre:str, fV: date, nL: int, fE:date, gO:str):
+    def __init__(self, nombre:str, fV: date, nL: int, fE:date, gO:str):        # Constructor
         self.__nombre = nombre
         self.__fechaVencimiento = fV
         self.__nroLote = nL
         self.__fechaEnvasado = fE
         self.__granjaOrigen = gO
 
+    # Getters    
     def get_nombre(self) -> str:
         return self.__nombre
+
     def getFechaVencimiento(self) -> date:
         return self.__fechaVencimiento
+    
     def getNumeroLote(self) -> int:
         return self.__nroLote
+    
     def getFechaEnvasado(self) -> date:
         return self.__fechaEnvasado
-    def getGranjaOrigen(self):
+    
+    def getGranjaOrigen(self):                   
         return self.__granjaOrigen
     
+    # Setters
     def setNombre(self, nuevoNombre: str):
         self.__nombre = nuevoNombre
-    def setNombre(self, nuevoFV: date):
+    
+    def setFechaVencimiento(self, nuevoFV: date):
         self.__fechaVencimiento = nuevoFV
-    def setNombre(self, nuevoNL: int):
+    
+    def setNumeroLote(self, nuevoNL: int):
         self.__nroLote = nuevoNL
-    def setNombre(self, nuevoFE: date):
+    
+    def setFechaEnvasado(self, nuevoFE: date):
         self.__fechaEnvasado = nuevoFE
-    def setNombre(self, nuevoGO: str):
+    
+    def setGranjaOrigen(self, nuevoGO: str):
         self.__granjaOrigen = nuevoGO
     
-    def getEtiqueta(self) -> str:
+
+    def getEtiqueta(self) -> str:    
         cadena = "| \t Producto: " + self.get_nombre + "\n"
         cadena += "| \t Fecha de vencimiento: " + str(self.getFechaVencimiento) + "\n"
         cadena += "| \t Numero de lote: " + str(self.getNumeroLote) + "\n"
