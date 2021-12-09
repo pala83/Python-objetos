5ee34b2e680c8cce5734c33f137f91a98e7c8f65
Juan Ignacio Rago <juanrago2015@gmail.com>
2021-12-09 19:59:41 -0300

    Se avanzó hasta el video 32

---
+++ Django_proyecto/ProyectoWeb/servicios/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models.fields import DateTimeField
+
+# Create your models here.
+
+class Servicio(models.Model):
+    titulo = models.CharField(max_length=50)    # texto
+    contenido = models.CharField(max_length=50) # texto
+    imagen = models.ImageField()                # imagen
+    created = DateTimeField(auto_now_add=True)  # fecha
+    updated = DateTimeField(auto_now_add=True)  # fecha
+
+    class Meta:
+        verbose_name='servicio'
+        verbose_name_plural='servicios'
+    
+    def __str__(self):
+        return self.titulo
